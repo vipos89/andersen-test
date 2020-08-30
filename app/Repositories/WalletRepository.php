67ae3983eef224332742078aa4d30388589bfe98
vipos89 <webmaster.kobrin@gmail.com>
2020-08-30 10:34:19 +0300

    fixes

--- app/Repositories/WalletRepository.php
+++ app/Repositories/WalletRepository.php
@@ -3,27 +3,13 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use App\Http\Resources\WalletResource;
 use App\Interfaces\RepositoryInterfaces\WalletInterface;
 use App\Models\Wallet;
 use App\Models\WalletTransaction;
-use App\Services\WalletService;
-use Exception;
 use Illuminate\Database\Eloquent\Collection;
 
 class WalletRepository implements WalletInterface
 {
-    /**
-     * @param int $userId
-     *
-     * @return Wallet
-     * @throws Exception
-     */
-    public function createWallet(int $userId): Wallet
-    {
-
-    }
-
     /**
      * Get info about wallet by hash
      *
